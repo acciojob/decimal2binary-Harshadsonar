840f600f135554f0b90c26ae1b13f1083a381e2e
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-03 19:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function decimalToBinary(num) {
-    // Write your code here
+function decimalToBinary(decimal) {
+  if (decimal === 0) {
+    return "0";
+  }
+
+  let binary = "";
+  while (decimal > 0) {
+    const remainder = decimal % 2;
+    binary = remainder.toString() + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary
+
